8f56e5f8b2e84a5c29d945314e791993de4a366c
Mohamad Amin Mirzaei <amin.mir1380@gmail.com>
2021-12-22 15:06:24 +0330

    change guzzlehttp version

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-
-use Thevil\RavandroApi\RavandroClient;
-
-$client = new RavandroClient("3|ciIDGbiBFuZdUQyJPKNhH8GxxKWcFNhoIpRMGCP3");
-
-var_dump($client->getLatestHH());
